0a8a1df8fec2918d82639b7eda1fc7342a55ed97
Web-Affan-Farooq <affanamir903@gmail.com>
2025-03-03 20:49:28 +0500

    updated unit convertor

--- unit-convertor-app/app.py
+++ unit-convertor-app/app.py
@@ -81,7 +81,68 @@ def convert_mass(from_unit,to_unit, value_tobe_converted):
     }
     result = (float(value_tobe_converted) * conversion_rates[to_unit])/ conversion_rates[from_unit]
     return result
-    
+
+def convert_volume(from_unit, to_unit, value_tobe_converted):
+              conversion_rates = {
+    # **Metric System (Base Unit: Liter)**
+    "Cubic meter": 1000,  "Cubic decimeter": 1,         # 1 dm³ = 1 L
+    "Cubic centimeter": 0.001,    # 1 cm³ = 0.001 L (1 mL)
+    "Cubic millimeter": 0.000001, # 1 mm³ = 0.000001 L
+    "Liter": 1,                   # Base unit
+    "Milliliter": 0.001,          # 1 mL = 0.001 L
+    "Hectoliter": 100,            # 1 hL = 100 L
+    "Kiloliter": 1000,            # 1 kL = 1000 L
+    "Megaliter": 1_000_000,       # 1 ML = 1,000,000 L
+
+    # **Imperial System**
+    "Imperial gallon": 4.54609,       # 1 imperial gallon = 4.54609 L
+    "Imperial quart": 1.13652,        # 1 imperial quart = 1.13652 L
+    "Imperial pint": 0.568261,        # 1 imperial pint = 0.568261 L
+    "Imperial cup": 0.284131,         # 1 imperial cup = 0.284131 L
+    "Imperial fluid ounce": 0.0284131,# 1 imperial fl oz = 0.0284131 L
+
+    # **US Customary System**
+    "US gallon": 3.78541,         # 1 US gallon = 3.78541 L
+    "US quart": 0.946353,         # 1 US quart = 0.946353 L
+    "US pint": 0.473176,          # 1 US pint = 0.473176 L
+    "US cup": 0.24,               # 1 US cup = 0.24 L
+    "US fluid ounce": 0.0295735,  # 1 US fl oz = 0.0295735 L
+
+    # **Other Units**
+    "Cubic inch": 0.0163871,      # 1 in³ = 0.0163871 L
+    "Cubic foot": 28.3168,        # 1 ft³ = 28.3168 L
+    "Cubic yard": 764.5549,       # 1 yd³ = 764.5549 L
+    "Oil barrel": 158.987,        # 1 oil barrel = 158.987 L
+    "Bushel": 35.2391             # 1 bushel = 35.2391 L
+}
+              result = float(value_tobe_converted)*(conversion_rates[from_unit] / conversion_rates[to_unit])
+              return result
+
+def convert_temperature(from_unit, to_unit, value_tobe_converted):
+        conversion_rates = {
+                "Celcius":0,
+                "Farenheit":32,
+                "Kelvin":273,
+        }
+        if(from_unit == "Celcius" and to_unit == "Farenheit"):
+                result = (float(value_tobe_converted)*9/5) + 32 
+                return result
+        elif(from_unit == "Celcius" and to_unit == "Kelvin"):
+                result = float(value_tobe_converted) + 273.15
+                return result
+        elif(from_unit == "Farenheit" and to_unit == "Celcius"):
+                result = (float(value_tobe_converted) - 32)* 5/9
+                return result
+        elif(from_unit == "Farenheit" and to_unit == "Kelvin"):
+                result = (float(value_tobe_converted) - 32) * 5/9 + 273.15 
+                return result
+        elif(from_unit == "Kelvin" and to_unit == "Celcius"):
+                result = float(value_tobe_converted) - 273.15
+                return result
+        elif(from_unit == "Kelvin" and to_unit == "Farenheit"):
+                result = (float(value_tobe_converted) - 273.15) * 9/5 + 32
+                return result
+        
 
 
 if(selected_option == "Length"):
@@ -135,8 +196,27 @@ elif(selected_option == "Mass"):
                 converted_value= st.text_area(to_unit,value=convert_mass(from_unit,to_unit, value_tobe_converted))
 
 elif(selected_option == "Volume"):
-    print(selected_option)
-    # _________Logic to be implemented
+                from_value = "1"
+                to_value = "1"
+                volume_options = ["Cubic meter","Cubic decimeter","Cubic centimeter","Cubic millimeter","Liter","Milliliter","Hectoliter","Kiloliter","Megaliter"
+,"Imperial gallon","Imperial quart","Imperial pint","Imperial cup","Imperial fluid ounce"
+,"Us gallon","Us quart","Us pint","Us cup","Us fluid ounce"
+,"Cubic inch","Cubic foot","Cubic yard","Oil barrel","Bushel"]
+                
+                from_unit = st.selectbox("From ", volume_options)
+                to_unit = st.selectbox("To", volume_options)
+
+                value_tobe_converted = st.text_area(from_unit,value=from_value)
+                converted_value= st.text_area(to_unit,value=convert_volume(from_unit, to_unit, value_tobe_converted))
+
 elif(selected_option == "Temperature"):
-    print(selected_option)
-    # _________Logic to be implementedelif(selected_option == "Pressure"):
+                from_value = "1"
+                to_value = "1"
+                temperature_options =["Celcius","Farenheit","Kelvin"]                
+                from_unit = st.selectbox("From ", temperature_options)
+                to_unit = st.selectbox("To", temperature_options)
+
+                value_tobe_converted = st.text_area(from_unit,value=from_value)
+                converted_value= st.text_area(to_unit,value=convert_temperature(from_unit,to_unit, value_tobe_converted))
+elif(selected_option == "Pressure"):
+        print(selected_option)
