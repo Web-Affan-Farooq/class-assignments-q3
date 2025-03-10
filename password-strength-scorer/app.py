36f05a0a22dd661d0bd0ede6219420a469f7acd2
Web-Affan-Farooq <affanamir903@gmail.com>
2025-03-10 22:09:24 +0500

    updated password strength calculator

--- password-strength-scorer/app.py
+++ password-strength-scorer/app.py
@@ -1,77 +1,46 @@
+from zxcvbn import zxcvbn
 import streamlit as st
-import re  # importing regex module
 
-st.title("Password strength calculator")
+st.title("Password Strength Calculator")
 
-def strength_calculator(password):
+password = st.text_input("Enter password")
 
-    # ____Initializing variables for score security measures and keywords ...
-    score = 0
-    security_measures = []
-    key_words = ""
+if password:
 
-    # ____Recording uppercase, lowercase  and special characters along with password length
-    length = len(password)
-    uppercase_letters = re.findall("[A-Z]",password)
-    number_chars = re.findall("[0-9]",password)
-    lower_letters = re.findall("[a-z]",password)
-    special_chars = re.findall(r"[!@#$%^&*()_\-+=\[\]{}<>?/.,~`:;|\\]",password)
+    # ____Eveluation using function
+    calculations = zxcvbn(password)
 
-    # ____Evaluating password with it's length
-    if(length >=8):
-        score+=1
-    else: security_measures.append("Ensure that the password contains 8 characters or long")   
+    # ___For debugging use this :
+    #  st.write(calculations)
 
-    # ____Evaluating passwords with uppercase characters
-    if(len(uppercase_letters)>=1):
-        score+=1
-    else: security_measures.append("A strong password must have 1 or 2 uppercase characters")
+    # ____Evaluation  
+    score = calculations["score"]
+    guess_attempts = calculations["guesses"]
 
-    # ____Evaluating password with lowercase characters
-    if(len(lower_letters)>=1):
-        score +=1
-    else: security_measures.append("Strong passwords must have atleast 1 lowercase character")
+    # ___Display ramarks
+    st.subheader("Remarks :")
+    if(score == 1): 
+         st.write("Vulnerable password . Zero strength and no security")
+    elif(score == 2): 
+         st.write("Very weak. Read security measures to fix this")
+    if(score == 3): 
+         st.write("Try improvements")
+    if(score == 4): 
+         st.write("Good !")
 
-    # ___Eveluating passwords with special characters
-    if(len(special_chars)>=2):
-          score +=1
-    else : security_measures.append("A strong password must contain atleast 2 special characters")
+    # ____Displaing status :
+    st.write(f"**Password Strength Score:** {score} / 4")
+    st.write("🔢 **Guess Attempts Required:**", guess_attempts)
+    st.write("⏳ **Crack Time (in seconds):** ", str(calculations["crack_times_seconds"]["offline_fast_hashing_1e10_per_second"]))
+
     
-    # ___Eveluating passwords with special characters
-    if(len(number_chars)>=2):
-          score +=1
-    else : security_measures.append("A strong password must contain atleast 2 numeric characters")
+    if(calculations["feedback"]["warning"]):
+        st.subheader("Warnings :")
+        st.warning(calculations["feedback"]["warning"])
 
-    # ____Generating keywords ...
-    if(score == 5):
-        key_words = "Strong password"
-    elif(score <5 and score >3):
-        key_words = "Moderate password"
-    elif(score <3 and score > 1):
-        key_words = "Very weak. Read security measures to fix this"
-    else: key_words = "Vulnerable password . Zero strength and no security"
-
-    return {
-        "Score":score,
-        "Security-measures":security_measures,
-        "Keywords":key_words
-    }
-
-password = st.text_input("Enter your password")
-calculated_result = strength_calculator(password)
-calculated_score = calculated_result["Score"]
-security_measures = calculated_result["Security-measures"]
-key_words = calculated_result["Keywords"]
-
-# ____Displaying outputs:
-st.title("Results")
-st.write("Score :", calculated_score)
-
-if(len(security_measures) == 0) :
-    st.write("No vulnurabilites found")
-else :
-    st.write("Security measures :")
-    for str in security_measures :
-        st.write(str)
-
-st.write("Keywords : ", key_words)
+    if(calculations["feedback"]["suggestions"]):
+        st.subheader("Suggestions and improvements:")
+        for i in range(len(calculations["feedback"]["suggestions"])):
+            st.write(f"- {calculations["feedback"]["suggestions"][i]}")
+else:
+    st.warning("⚠️ Please enter a password!")
---
+++ password-strength-scorer/readme.md
@@ -0,0 +1,9 @@
+## Apis for handling passwords :
+- [Open AI](https://openai.com/api/)
+
+In this app we are using **zxcvbn-python**, a library for deadling with password security testing 
+install it as :
+
+```bash 
+pip install zxcvbn-python
+```
